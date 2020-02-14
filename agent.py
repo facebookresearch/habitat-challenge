e91054656acbe8af450b6b540920558c778795af
Oleksandr Maksymets <maksymets@gmail.com>
2020-02-13 16:31:41 -0800

    Added random agent

--- agent.py
+++ agent.py
@@ -1,18 +1,14 @@
 import argparse
 import habitat
-import random 
 
 NUM_ACTIONS = 6
 
 class RandomAgent(habitat.Agent):
     def act(self, observations):
-        return random.randint(NUM_ACTIONS) 
-
-
-
+        return random.randint(NUM_ACTIONS)
 
 def main():
-    agent = RandomAgent()
+    agent = RandomAgent
     challenge = habitat.Challenge()
     challenge.submit(agent)
 
