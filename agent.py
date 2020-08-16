23b3ca554e8964ff82e1079891b29c6387b7683d
Oleksandr Maksymets <maksymets@gmail.com>
2020-08-15 19:18:36 -0700

    Black reformatting

--- agent.py
+++ agent.py
@@ -18,7 +18,9 @@ class RandomAgent(habitat.Agent):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--evaluation", type=str, required=True, choices=["local", "remote"])
+    parser.add_argument(
+        "--evaluation", type=str, required=True, choices=["local", "remote"]
+    )
     args = parser.parse_args()
 
     config_paths = os.environ["CHALLENGE_CONFIG_FILE"]
@@ -35,4 +37,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
